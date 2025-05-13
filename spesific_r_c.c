deab549e48a4751deb9af4c6331a5ed5fd1f9819
Nasim <mdnasimhosen333@gmail.com>
2025-05-13 19:40:00 +0600

    Update input and output files; fix specific row access in main function

--- spesific_r_c.c
+++ spesific_r_c.c
@@ -1,3 +1,28 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int r, c;
+//     scanf("%d %d", &r, &c);
+//     int a[r][c];
+//     for (int i = 0; i < r; i++)
+//     {
+//         for (int j = 0; j < c; j++)
+//         {
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     int specific;
+//     scanf("%d", &specific);
+
+//     for (int j = 0; j < c; j++)
+//     {
+//         printf("%d ", a[specific][j]);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
@@ -16,9 +41,9 @@ int main()
     int specific;
     scanf("%d", &specific);
 
-    for (int j = 0; j < c; j++)
+    for (int j = 0; j < r; j++)
     {
-        printf("%d ", a[specific][j]);
+        printf("%d ", a[j][specific]);
     }
     return 0;
 }
